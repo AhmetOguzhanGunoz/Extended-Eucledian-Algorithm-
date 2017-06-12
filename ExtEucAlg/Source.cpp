c994c2a63edde1557f9e31f4bb006458dca28646
AhmetOguzhanGunoz <oguzhangunoz@gmail.com>
2017-06-12 18:45:10 +0300

    Extended Eucledian Algorithm Console Application
    This console application helps to find reverse module of two numbers. Inputs must have greatest common divisor as 1. Application displays result like a operation which multiplying inputs with numbers to get greatest common divisor. These multiplier numbers are reverse module results of two inputs.

---
+++ ExtEucAlg/Source.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <stdio.h> 
+#include <stdlib.h> 
+
+using namespace std;
+
+int main(void)
+{
+	int inputA, inputB, a0, b0, q, r, temp;
+
+	int  t0 = 0,
+		t = 1,
+		s0 = 1,
+		s = 0;
+
+	cout << "Enter 1st number to find index:";
+
+	cin >> inputA;
+
+	cout << "Enter 2nd number to find index:";
+
+	cin >> inputB;
+
+	cout << "\n";
+
+	a0 = inputA;
+
+	b0 = inputB;
+
+	q = inputA / inputB;
+
+	r = a0 - q * b0;
+
+
+	do
+	{
+
+		temp = t0 - q*t;
+		t0 = t;
+		t = temp;
+		temp = s0 - q*s;
+		s0 = s;
+		s = temp;
+		a0 = b0;
+		b0 = r;
+		q = a0 / b0;
+		r = a0 - q*b0;
+	} while (r > 0);
+
+	cout << b0 << " = gcd(" << inputA << " , " << inputB << ") and " << s << " . " << inputA << " + " << t << " . " << inputB << " = " << b0 << "\n\n";
+
+	//r = 0 çıktığı için yazdırmada bir önceki r yi yani b0 ı yazdırıyoruz.
+	
+	system("pause");
+
+
+
+	return 0;
+}
